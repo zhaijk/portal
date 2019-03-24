f263720e7a38d2805dc60905999c62aed002038e
zhaijk <zhaijk.hotmail.com>
2019-03-25 07:04:51 +0800

    创建工程

---
+++ src/main/java/com/sxds/portal/domain/CartProduct.java
@@ -0,0 +1,33 @@
+package com.sxds.portal.domain;
+
+
+
+import java.util.List;
+
+import com.macro.mall.model.PmsProduct;
+import com.macro.mall.model.PmsProductAttribute;
+
+/**
+ * 购物车中选择规格的商品信息
+ * Created by macro on 2018/8/2.
+ */
+public class CartProduct extends PmsProduct {
+    private List<PmsProductAttribute> productAttributeList;
+    private List<PmsSkuStock> skuStockList;
+
+    public List<PmsProductAttribute> getProductAttributeList() {
+        return productAttributeList;
+    }
+
+    public void setProductAttributeList(List<PmsProductAttribute> productAttributeList) {
+        this.productAttributeList = productAttributeList;
+    }
+
+    public List<PmsSkuStock> getSkuStockList() {
+        return skuStockList;
+    }
+
+    public void setSkuStockList(List<PmsSkuStock> skuStockList) {
+        this.skuStockList = skuStockList;
+    }
+}
